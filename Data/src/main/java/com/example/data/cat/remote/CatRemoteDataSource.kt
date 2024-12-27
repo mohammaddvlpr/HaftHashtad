f454a989a4cf05b7a76534cc2f40f114adff64ad
mohammad <mohammaddvlpr@gmail.com>
2024-12-28 01:19:05 +0330

    Added a function to CatRemoteDataSource and its impl to get cat detail by id

--- Data/src/main/java/com/example/data/cat/remote/CatRemoteDataSource.kt
+++ Data/src/main/java/com/example/data/cat/remote/CatRemoteDataSource.kt
@@ -1,10 +1,13 @@
 package com.example.data.cat.remote
 
 import com.example.data.cat.remote.models.CatApiModel
+import com.example.data.cat.remote.models.CatDetailApiModel
 
 
 interface CatRemoteDataSource {
 
     suspend fun getCats(page:Int): Result<List<CatApiModel>>
+
+    suspend fun getCatById(id:String):Result<CatDetailApiModel>
 
 }
--- Data/src/main/java/com/example/data/cat/remote/CatRemoteDataSourceImpl.kt
+++ Data/src/main/java/com/example/data/cat/remote/CatRemoteDataSourceImpl.kt
@@ -2,12 +2,18 @@ package com.example.data.cat.remote
 
 import com.example.data.apiCall
 import com.example.data.cat.remote.models.CatApiModel
+import com.example.data.cat.remote.models.CatDetailApiModel
 import javax.inject.Inject
 
 class CatRemoteDataSourceImpl @Inject constructor(private val catService: CatService) :
     CatRemoteDataSource {
     override suspend fun getCats(page: Int): Result<List<CatApiModel>> {
         return apiCall(call = { catService.getCats(page) },
+            map = { it })
+    }
+
+    override suspend fun getCatById(id: String): Result<CatDetailApiModel> {
+        return apiCall(call = { catService.getCatDetailById(id) },
             map = { it })
     }
 }
