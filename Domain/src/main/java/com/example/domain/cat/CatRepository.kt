a043cc541a08de279304fef299a890a624d89e73
mohammad <mohammaddvlpr@gmail.com>
2024-12-28 01:04:40 +0330

    Created CatDetailModel Added a function to CatRepository to get cat by id

--- Domain/src/main/java/com/example/domain/cat/CatRepository.kt
+++ Domain/src/main/java/com/example/domain/cat/CatRepository.kt
@@ -1,11 +1,15 @@
 package com.example.domain.cat
 
 import androidx.paging.PagingData
+import com.example.domain.cat.models.CatDetailModel
 import com.example.domain.cat.models.CatModel
 import kotlinx.coroutines.flow.Flow
 
 interface CatRepository {
 
     fun getCatsPagingFlow(): Flow<PagingData<CatModel>>
+
+    suspend fun getCatDetailById(id:String): Result<CatDetailModel>
+
 
 }
---
+++ Domain/src/main/java/com/example/domain/cat/models/CatDetailModel.kt
@@ -0,0 +1,8 @@
+package com.example.domain.cat.models
+
+data class CatDetailModel(
+    val id: String,
+    val imageUrl: String,
+    val isFavourite: Boolean,
+    val name: String
+)
