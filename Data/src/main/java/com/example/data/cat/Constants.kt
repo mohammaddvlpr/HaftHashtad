0a2c8296500b349c12a776c92d573db2dbe4fdc1
mohammad <mohammaddvlpr@gmail.com>
2024-12-27 19:15:01 +0330

    Added a file for api constants Changed CatService and replaced hardcoded page size

---
+++ Data/src/main/java/com/example/data/cat/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.data.cat
+
+const val PAGE_SIZE = 20
--- Data/src/main/java/com/example/data/cat/remote/CatService.kt
+++ Data/src/main/java/com/example/data/cat/remote/CatService.kt
@@ -1,12 +1,13 @@
 package com.example.data.cat.remote
 
+import com.example.data.cat.PAGE_SIZE
 import com.example.data.cat.remote.models.CatApiModel
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface CatService {
 
-    @GET("images/search?limit=10&has_breeds=1&order=DESC")
+    @GET("images/search?limit=$PAGE_SIZE&has_breeds=1&order=DESC")
     suspend fun getCats(
         @Query("page") page: Int
     ): List<CatApiModel>
