c038f849e7e6a587e06bde0a5f71f37b63f8aea4
mohammad <mohammaddvlpr@gmail.com>
2024-12-27 23:55:38 +0330

    Added SaveStateHandle to read the param that is passed to screen

--- app/src/main/java/com/example/hafthashtad/screen/detail/DetailScreenViewModel.kt
+++ app/src/main/java/com/example/hafthashtad/screen/detail/DetailScreenViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.hafthashtad.screen.detail
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.hafthashtad.navigation.ID
 import com.example.hafthashtad.screen.detail.models.DetailScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -9,10 +11,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DetailScreenViewModel @Inject constructor(
-) :
+    savedStateHandle: SavedStateHandle,
+    ) :
     ViewModel() {
     private val _state = MutableStateFlow(DetailScreenState())
     val state: StateFlow<DetailScreenState> = _state
+
+    private val id: String? = savedStateHandle[ID]
+
 
 
 }
