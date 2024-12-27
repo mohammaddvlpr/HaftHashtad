d87f2b5d336660467c537f4b497e23c9bcc99f6b
mohammad <mohammaddvlpr@gmail.com>
2024-12-28 03:04:26 +0330

    Added a function to FavouriteCatIdsDao to return flow of ids

--- Data/src/main/java/com/example/data/cat/local/FavouriteCatIdsDao.kt
+++ Data/src/main/java/com/example/data/cat/local/FavouriteCatIdsDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.data.cat.local.entity.FavouriteCatIdEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavouriteCatIdsDao {
@@ -21,5 +22,6 @@ interface FavouriteCatIdsDao {
     @Query("SELECT COUNT(*) > 0 FROM favourite_cat_id_entity WHERE id = :id")
     suspend fun isFavourite(id:String): Boolean
 
-
+    @Query("SELECT id FROM favourite_cat_id_entity")
+    fun getAllFavouritesCatIdsFlow(): Flow<List<String>>
 }
