cade2a155876b18b1a2ab18c531b06a644b0cb5f
mohammad <mohammaddvlpr@gmail.com>
2024-12-28 01:32:06 +0330

    Changed DetailScreenViewModel to get detail data from domain layer Implemented DetailScreenMapper

--- app/src/main/java/com/example/hafthashtad/screen/detail/DetailScreenViewModel.kt
+++ app/src/main/java/com/example/hafthashtad/screen/detail/DetailScreenViewModel.kt
@@ -2,23 +2,49 @@ package com.example.hafthashtad.screen.detail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.cat.useCase.GetCatDetailByIDUseCase
 import com.example.hafthashtad.navigation.ID
+import com.example.hafthashtad.screen.detail.models.DetailScreenMapper
 import com.example.hafthashtad.screen.detail.models.DetailScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailScreenViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    ) :
+    private val getCatDetailByIDUseCase: GetCatDetailByIDUseCase,
+    private val detailScreenMapper: DetailScreenMapper
+) :
     ViewModel() {
     private val _state = MutableStateFlow(DetailScreenState())
     val state: StateFlow<DetailScreenState> = _state
 
     private val id: String? = savedStateHandle[ID]
 
+    init {
+        viewModelScope.launch {
+            if (id != null) {
+                val result = getCatDetailByIDUseCase(id)
+                val resultValue = result.getOrNull()
+
+                if (resultValue != null)
+                    _state.update {
+                        it.copy(
+                            newsDetailUiModel = detailScreenMapper.mapNewModelToNewsDetailUiModel(
+                                resultValue
+                            )
+                        )
+                    }
+            }
+
+
+        }
+    }
 
 
 }
---
+++ app/src/main/java/com/example/hafthashtad/screen/detail/models/DetailScreenMapper.kt
@@ -0,0 +1,11 @@
+package com.example.hafthashtad.screen.detail.models
+
+import com.example.domain.cat.models.CatDetailModel
+import javax.inject.Inject
+
+class DetailScreenMapper @Inject constructor() {
+
+    fun mapNewModelToNewsDetailUiModel(model: CatDetailModel) = with(model) {
+        CatDetailUiModel(id, name, imageUrl, description)
+    }
+}
